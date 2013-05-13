c7581555765b5011baa31884bc77b158d8b5b1e2
Brett Viren <bv@bnl.gov>
2013-05-13 12:36:29 -0400

    Lightness before the dark.

--- init-theme.el
+++ init-theme.el
@@ -3,7 +3,8 @@
 (require-package 'color-theme-solarized)
 ;;;; theme
 ;; https://github.com/sellout/emacs-color-theme-solarized
-(load-theme 'solarized-dark t)
+;(load-theme 'solarized-dark t)
+(load-theme 'solarized-light t)
 
 (provide 'init-theme)
 
